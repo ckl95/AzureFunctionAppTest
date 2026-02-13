0205f8af1ee8861212874567fb1a41ca922d21ab
Casper KLAVER <casper.klaver@soprasteria.com>
2026-02-13 13:20:54 +0100

    Commit

--- function_app.py
+++ function_app.py
@@ -1,23 +1,32 @@
+
 import azure.functions as func
 import logging
+import os
 import json
-import urllib.request
-import urllib.parse
+import re
+from typing import Any, Dict, List
+from collections import defaultdict
+import pandas as pd
+from azure.identity import DefaultAzureCredential
+from azure.storage.filedatalake import DataLakeServiceClient
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.route(route="http_trigger", methods=["GET"])
+@app.route(route="http_trigger", methods=["POST"])
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request (Graph → normalized JSON array)")
 
-    all_rows = []
-    all_rows.append({"yolo":"Zo ouderwets"})
 
-    # Return ONLY the array of nice flat rows
+    try:
+        body = req.get_json()
+    except ValueError:
+        return func.HttpResponse("Invalid or missing JSON body.", status_code=400)
     return func.HttpResponse(
         json.dumps(all_rows, ensure_ascii=False),
         mimetype="application/json",
         status_code=200
     )
+
+
